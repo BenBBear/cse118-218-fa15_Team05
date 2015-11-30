9bdcf300eeb0017b1eba540c61fe3ae7be445e48
Xinyu <xyzhang.me@aliyun.com>
2015-11-29 18:04:51 -0800

    Push the data into database and insert points into database

--- ergoTracker/db/db_actions.js
+++ ergoTracker/db/db_actions.js
@@ -1,23 +1,33 @@
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var ObjectId = require('mongodb').ObjectID;
-var url ='mongodb://localhost:27017/ErgoDB';
-var partPoints = 'partPoints';
-var insertPoints = function(data, callback) {
-       //data consistes userId and their points
-       MongoClient.connect(url, function(err, db){
-           assert.equal(null,err);
-           //form points json
-           db.collection(partPoints).insertOne( {
-                 "userId" : data.userId,
-                 "points" : data.points,
-                 "date" : new Date().getTime(),
-                  }
-                , function(err, result) {
-                               assert.equal(err, null);
-                               console.log("Inserted a document into the partPoints collection.");
-                               callback(result);
-                          });
-        });
-};
-exports.insertPoints = insertPoints
+var ip = '52.89.152.186';
+var url = 'mongodb://'+ip+ ':27017/ErgoDB';
+var partPoints = 'PartPoints';
+
+var mongoose = require('mongoose');
+
+mongoose.connect(url);
+var PartPoints = mongoose.model('PartPoints', {
+    userId: String,
+    date: Date,
+    score:Number,
+    points: mongoose.Schema.Types.Mixed
+});
+
+
+function insertPoints(data,score, cb) {
+    var p = new PartPoints({
+        "userId": data.userId,
+        "points": data.points,
+        "score":score,
+        "date": new Date().getTime()
+    });
+    p.save(function (err) {
+        if (err)
+            cb(err);
+        else
+            cb(null);
+    });
+}
+
+
+
+exports.insertPoints = insertPoints;
--- ergoTracker/routes/index.js
+++ ergoTracker/routes/index.js
@@ -1,64 +1,106 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../db/db_actions.js');
-var avg_score = Math.round(Math.random() * 100)/100 // This is cached score
-var num = 1;
+
+var bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded 
+router.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// parse application/json 
+router.use(bodyParser.json());
+
+
 /* GET diagnostic data. */
 router.get('/get/diagnostic_data', function(req, res, next) {
-  var fromDate = req.query.fromDate;
-  var toDate = req.query.toDate;
-  var userId = req.query.userId;;
-  var date = new Date();
-  var prevd = new Date();
-  //assert for the data types
-  if(typeof(fromDate) == 'undefined' || typeof(toDate) == 'undefined' || typeof(userId) == 'undefined'){
-    res.status(500);
-    res.send("One of [fromDate, toDate, userId undefined");
-    return;
-  }
-  //calculate the yesterday's date
-  prevd.setTime(date.getTime() - 1000*60*60*24);
-  var score_arr = {'scores': [{'date':prevd.toDateString(), 'score':41.25},{'date':date.toDateString(), 'score': 65}]} 
+    var fromDate = req.query.fromDate;
+    var toDate = req.query.toDate;
+    var userId = req.query.userId;;
+    var date = new Date();
+    var prevd = new Date();
+    //assert for the data types
+    if (typeof(fromDate) == 'undefined' || typeof(toDate) == 'undefined' || typeof(userId) == 'undefined') {
+        res.status(500);
+        res.send("One of [fromDate, toDate, userId undefined");
+        return;
+    }
+    //calculate the yesterday's date
+    prevd.setTime(date.getTime() - 1000 * 60 * 60 * 24);
+    var score_arr = {
+        'scores': [{
+            'date': prevd.toDateString(),
+            'score': 41.25
+        }, {
+            'date': date.toDateString(),
+            'score': 65
+        }]
+    }
 
-  res.status(404);
-  res.send(score_arr);
+    res.status(404);
+    res.send(score_arr);
 });
 
 /* POST kinect data. */
-router.post('/post/kinect_data', function(req, res, next) {
-  
-    var err = { "message": "Data Inserted into database",
-                "code": "0"
-              };
-    //post is a passive call.
-    //If there are some points missing 
-    //then we donot bother about it
-    var score = Math.round(Math.random()*100)/100;
-    avg_score = ((avg_score*num++) + score)/num; //post incrementing number to calculate previous sum   
-    var result = {"error": err,
-       "score":score,
-       "break":true,
-       "avg_score":avg_score.toFixed(2), //so far today
-  }
-  res.status(200);
-  res.send(result);
+
+var algorithm = require('2015-cse218-group5-algorithm');
+var cache = require('memory-cache');
+algorithm.FrameRate(5);
+
+
+
+router.post('/post/kinect_data', function(req, res, next) {    
+    var num, avg_score,result;
+    algorithm(req.body.points, function(err, score, isBreak) {
+        if (cache.get(req.body.userId)) {
+            var t = cache.get(req.body.userId);
+            num = t.num;
+            avg_score = t.avg_score;
+            avg_score = ((avg_score * num++) + score) / num;
+        } else {
+            cache.put(req.body.userId,{
+                num:1,
+                avg_score:score
+            }, 1000*60*60);
+            avg_score = score;
+        }
+        result = {
+            "error": null,
+            "score": score,
+            "break": isBreak,
+            "avg_score": avg_score.toFixed(2) //so far today
+        };        
+        db.insertPoints(req.body, score , function(err) {
+            if (err)
+                res.json({
+                    "message": "Datebase Error",
+                    "code": 1
+                });
+            else {                
+                res.json(result);
+            }
+        });        
+    });        
 });
+
 
 /* GET today's score. */
 router.get('/get/today_score', function(req, res, next) {
-  var date = new Date()
-  var score = Math.round(Math.random()*100)/100
-  var userId = req.query.userId;
-  if (typeof(userId) == "undefined"){
-      res.status(500);
-      res.send("userId not sent");
-      return;
-  }
-  res.status(200);
-  res.send(score.toFixed(2))
+    var date = new Date()
+    var score = Math.round(Math.random() * 100) / 100
+    var userId = req.query.userId;
+    if (typeof(userId) == "undefined") {
+        res.status(500);
+        res.send("userId not sent");
+        return;
+    }
+    res.status(200);
+    res.send(score.toFixed(2))
 });
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
 });
 module.exports = router;
